5251a1a235b5676aa31c25b0fa2f92981bc24891
ShinYounHo <suh2963@gmail.com>
2025-02-05 14:56:29 +0900

    delete -r

--- src/test/java/com/example/rest_tdd/ApiV1PostControllerTest.java
+++ src/test/java/com/example/rest_tdd/ApiV1PostControllerTest.java
@@ -45,18 +45,10 @@ public class ApiV1PostControllerTest {
                 .andExpect(jsonPath("$.data.modifiedDate").value(matchesPattern(post.getModifiedDate().toString().replaceAll("0+$", "") + ".*")));
     }
 
-    private ResultActions itemRequest(long postId) throws Exception {
-
-        return mvc.perform(
-                        get("/api/v1/posts/%d".formatted(postId))
-                )
-                .andDo(print());
-    }
-
-    private ResultActions modifyRequest(long postId, String apiKey, String title, String content) throws Exception {
+    private ResultActions writeRequest(String apiKey, String title, String content) throws Exception {
 
         ResultActions resultActions = mvc.perform(
-                        put("/api/v1/posts/%d".formatted(postId))
+                        post("/api/v1/posts")
                                 .header("Authorization", "Bearer " + apiKey)
                                 .content("""
                                         {
@@ -72,6 +64,33 @@ public class ApiV1PostControllerTest {
                 .andDo(print());
 
         return resultActions;
+    }
+
+    private ResultActions itemRequest(long postId) throws Exception {
+
+        return mvc.perform(
+                        get("/api/v1/posts/%d".formatted(postId))
+                )
+                .andDo(print());
+    }
+
+    private ResultActions modifyRequest(long postId, String apiKey, String title, String content) throws Exception {
+
+        return mvc.perform(
+                        put("/api/v1/posts/%d".formatted(postId))
+                                .header("Authorization", "Bearer " + apiKey)
+                                .content("""
+                                        {
+                                            "title": "%s",
+                                            "content": "%s"
+                                        }
+                                        """.formatted(title, content)
+                                        .stripIndent())
+                                .contentType(
+                                        new MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8)
+                                )
+                )
+                .andDo(print());
     }
 
     @Test
@@ -103,27 +122,6 @@ public class ApiV1PostControllerTest {
                 .andExpect(handler().methodName("getItem"))
                 .andExpect(jsonPath("$.code").value("404-1"))
                 .andExpect(jsonPath("$.msg").value("존재하지 않는 글입니다."));
-    }
-
-    private ResultActions writeRequest(String apiKey, String title, String content) throws Exception {
-
-        ResultActions resultActions = mvc.perform(
-                        post("/api/v1/posts")
-                                .header("Authorization", "Bearer " + apiKey)
-                                .content("""
-                                        {
-                                            "title": "%s",
-                                            "content": "%s"
-                                        }
-                                        """.formatted(title, content)
-                                        .stripIndent())
-                                .contentType(
-                                        new MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8)
-                                )
-                )
-                .andDo(print());
-
-        return resultActions;
     }
 
     @Test
@@ -260,5 +258,29 @@ public class ApiV1PostControllerTest {
                 .andExpect(handler().methodName("modify"))
                 .andExpect(jsonPath("$.code").value("403-1"))
                 .andExpect(jsonPath("$.msg").value("자신이 작성한 글만 수정 가능합니다."));
+    }
+
+    private ResultActions deleteRequest(long postId, String apiKey) throws Exception {
+
+        return mvc.perform(
+                        delete("/api/v1/posts/%d".formatted(postId))
+                                .header("Authorization", "Bearer " + apiKey))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("글 삭제")
+    void delete1() throws Exception {
+
+        long postId = 1;
+        String apiKey = "user1";
+
+        ResultActions resultActions = deleteRequest(postId, apiKey);
+
+        resultActions.andExpect(status().isOk())
+                .andExpect(handler().handlerType(ApiV1PostController.class))
+                .andExpect(handler().methodName("delete"))
+                .andExpect(jsonPath("$.code").value("200-1"))
+                .andExpect(jsonPath("$.msg").value("%d번 글 삭제가 완료되었습니다.".formatted(postId)));
     }
 }
