87ac99f63d888661e3f784e51163a5c27b47364e
ShinYounHo <suh2963@gmail.com>
2025-02-05 12:11:30 +0900

    post get item - r

--- src/test/java/com/example/rest_tdd/ApiV1PostControllerTest.java
+++ src/test/java/com/example/rest_tdd/ApiV1PostControllerTest.java
@@ -1,11 +1,21 @@
 package com.example.rest_tdd;
 
+import com.example.rest_tdd.domain.post.post.entity.Post;
+import com.example.rest_tdd.domain.post.post.service.PostService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.hamcrest.text.MatchesPattern.matchesPattern;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -15,4 +25,40 @@ public class ApiV1PostControllerTest {
 
     @Autowired
     private MockMvc mvc;
+
+    @Autowired
+    private PostService postService;
+
+    private void checkPost(ResultActions resultActions, Post post) throws Exception {
+
+        resultActions.andExpect(jsonPath("$.data").exists())
+                .andExpect(jsonPath("$.data.id").value(post.getId()))
+                .andExpect(jsonPath("$.data.title").value(post.getTitle()))
+                .andExpect(jsonPath("$.data.content").value(post.getContent()))
+                .andExpect(jsonPath("$.data.authorId").value(post.getAuthor().getId()))
+                .andExpect(jsonPath("$.data.authorName").value(post.getAuthor().getNickname()))
+                .andExpect(jsonPath("$.data.createdDate").value(matchesPattern(post.getCreatedDate().toString().replaceAll("0+$", "") + ".*")))
+                .andExpect(jsonPath("$.data.modifiedDate").value(matchesPattern(post.getModifiedDate().toString().replaceAll("0+$", "") + ".*")));
+    }
+
+    @Test
+    @DisplayName("글 단건 조회")
+    void item() throws Exception {
+
+        long postId = 1;
+
+        ResultActions resultActions = mvc.perform(
+                        get("/api/v1/posts/%d".formatted(postId))
+                )
+                .andDo(print());
+
+        resultActions.andExpect(status().isOk())
+                .andExpect(handler().handlerType(ApiV1PostControllerTest.class))
+                .andExpect(handler().methodName("getItem"))
+                .andExpect(jsonPath("$.code").value("200-1"))
+                .andExpect(jsonPath("$.msg").value("%d번 글을 조회하였습니다.".formatted(postId)));
+
+        Post post = postService.getItem(postId).get();
+        checkPost(resultActions, post);
+    }
 }
